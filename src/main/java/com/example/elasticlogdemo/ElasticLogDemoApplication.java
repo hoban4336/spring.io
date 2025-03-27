68a54a6a783aea5e41c9a600b679a3f4222cf9c8
ysyoo <hoban4336@gmail.com>
2025-03-27 09:32:02 +0900

    init spring.io

---
+++ src/main/java/com/example/elasticlogdemo/ElasticLogDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticlogdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticLogDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticLogDemoApplication.class, args);
+	}
+
+}
